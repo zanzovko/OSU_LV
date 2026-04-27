4f1b1072a0af466a136aa9d0af7f3a03bc0d3f00
zanzovko <zan.zovko2004@gmail.com>
2026-04-27 09:22:04 +0200

    Dodano LV9

---
+++ LV9/9.4.3.py
@@ -0,0 +1,64 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.utils import to_categorical
+
+# Učitaj CIFAR-10
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+
+# Skaliraj na [0,1]
+X_train_n = X_train.astype('float32') / 255.0
+X_test_n = X_test.astype('float32') / 255.0
+
+# 1-od-K kodiranje
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+# CNN s DROPOUT slojevima
+model = keras.Sequential()
+model.add(layers.Input(shape=(32, 32, 3)))
+
+model.add(layers.Conv2D(32, (3, 3), activation='relu', padding='same'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Dropout(0.25))
+
+model.add(layers.Conv2D(64, (3, 3), activation='relu', padding='same'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Dropout(0.25))
+
+model.add(layers.Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Dropout(0.25))
+
+model.add(layers.Flatten())
+model.add(layers.Dense(500, activation='relu'))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(10, activation='softmax'))
+
+model.summary()
+
+# CALLBACKS s ranim zaustavljanjem
+my_callbacks = [
+    keras.callbacks.EarlyStopping(monitor='val_loss',         # prati validacijski gubitak
+                                  patience=5,                 # 5 epoha bez poboljšanja
+                                  verbose=1,
+                                  restore_best_weights=True)  # vrati najbolje težine
+]
+
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(X_train_n, y_train,
+          epochs=40,
+          batch_size=64,
+          callbacks=my_callbacks,
+          validation_split=0.1)
+
+score = model.evaluate(X_test_n, y_test, verbose=0)
+print(f'Točnost na testnom skupu: {100.0*score[1]:.2f}%')
---
+++ LV9/Zadatak_1_template.py
@@ -0,0 +1,67 @@
+import os
+
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.utils import to_categorical
+from matplotlib import pyplot as plt
+
+
+# ucitaj CIFAR-10 podatkovni skup
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+
+# prikazi 9 slika iz skupa za ucenje
+plt.figure()
+for i in range(9):
+    plt.subplot(330 + 1 + i)
+    plt.xticks([]),plt.yticks([])
+    plt.imshow(X_train[i])
+
+plt.show()
+
+
+# pripremi podatke (skaliraj ih na raspon [0,1]])
+X_train_n = X_train.astype('float32')/ 255.0
+X_test_n = X_test.astype('float32')/ 255.0
+
+# 1-od-K kodiranje
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+# CNN mreza
+model = keras.Sequential()
+model.add(layers.Input(shape=(32,32,3)))
+model.add(layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(layers.Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(500, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+model.summary()
+
+# definiraj listu s funkcijama povratnog poziva
+my_callbacks = [
+    keras.callbacks.TensorBoard(log_dir=os.path.join('logs', 'cnn'),
+                            update_freq=100)
+]
+
+model.compile(optimizer='adam',
+                loss='categorical_crossentropy',
+                metrics=['accuracy'])
+
+model.fit(X_train_n,
+            y_train,
+            epochs = 40,
+            batch_size = 64,
+            #callbacks = my_callbacks,
+            validation_split = 0.1)
+
+
+score = model.evaluate(X_test_n, y_test, verbose=0)
+print(f'Tocnost na testnom skupu podataka: {100.0*score[1]:.2f}')
+
